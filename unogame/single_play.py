84d8bcdd3ac7e50375146f754b1bd7d95e354b54
소태호 <sth9057@gmail.com>
2023-03-26 22:40:23 +0900

    reverse 작동 안함 버튼이 두번 눌리는 현상 all change 작동하지 않음 - 확실하지 않음 테스트 데이터가 필요함

--- unogame/single_play.py
+++ unogame/single_play.py
@@ -38,11 +38,21 @@ class Card(pygame.sprite.Sprite):
         self.card_state = False  # 앞뒷면을 나타내는 변수, True = 앞면 / False = 뒷면
 
 
+def draw_card(input_deck):
+    global deck, turn_index, turn_list
+    temp_list = deck.sprites()
+    random.shuffle(temp_list)
+    pop_card = temp_list.pop()
+    deck.remove(pop_card)
 
+    if turn_index == 0:
+        pop_card.rect.y = 450  # 200 - rect_height // 2
+        pop_card.initial_y = 450
+    elif turn_index == 1:
+        pop_card.rect.y = 50  # 200 - rect_height // 2
+        pop_card.initial_y = 50
+    input_deck.add(pop_card)
 
-def draw_two():  # deck : list / first, second : card
-
-    return deck
 
 
 def block_turn():
@@ -58,36 +68,51 @@ def reverse_turn():
 
 
 def change_color():
-    global screen, deck, remain, is_color_change, game_active, now_card_surf, change_color_list
+    global is_color_change
     if is_color_change:
         is_color_change = False
         return 0
     is_color_change = True
-    change_color_list = [[red_surf, red_rect, "red"], [green_surf, green_rect, 'green'], [blue_surf, blue_rect, 'blue'],
-                         [yellow_surf, yellow_rect, 'yellow']]
-
-    return 0
 
 
-def wild_draw_four():
-    return 0
+def draw_two(input_deck, next_player):  # deck : list / first, second : card
+    global deck, turn_index, turn_list
+    for _ in range(2):
+        temp_list = deck.sprites()
+        random.shuffle(temp_list)
+        pop_card = temp_list.pop()
+        deck.remove(pop_card)
+
+        if next_player == 0:
+            pop_card.rect.y = 450  # 200 - rect_height // 2
+            pop_card.initial_y = 450
+        elif next_player == 1:
+            pop_card.rect.y = 50  # 200 - rect_height // 2
+            pop_card.initial_y = 50
+
+        input_deck.add(pop_card)
+
+    hand_update(input_deck)
 
 
-def setting(num_players):
-    global turn_index
+def draw_four(input_deck, next_player):
+    global deck, turn_index, turn_list
+    for _ in range(4):
+        temp_list = deck.sprites()
+        random.shuffle(temp_list)
+        pop_card = temp_list.pop()
+        deck.remove(pop_card)
 
-    # 인원 수 만큼 차례 추가
-    for i in range(0, num_players):
-        turn_list.append(i)
-    # 시작 할 사람 결정
-    turn_index = random.randint(0, 4)
-    return turn_list
+        if next_player == 0:
+            pop_card.rect.y = 450  # 200 - rect_height // 2
+            pop_card.initial_y = 450
+        elif next_player == 1:
+            pop_card.rect.y = 50  # 200 - rect_height // 2
+            pop_card.initial_y = 50
 
+        input_deck.add(pop_card)
 
-def draw_card(deck):
-    global center_card_list
-    deck.append(center_card_list.pop())
-
+    hand_update(input_deck)
 
 def hand_update(input_deck):
     for i, card in enumerate(input_deck.sprites()):
@@ -97,7 +122,7 @@ def hand_update(input_deck):
 
 
 def generate_deck():
-    global deck, remain, now_card_surf, now_card_rect
+    global deck, remain, now_card_surf, now_card_rect, now_card
     count = 0
     for color, number in itertools.product(colors, numbers):
         deck.add(Card(color, number, None, False))
@@ -119,40 +144,28 @@ def generate_deck():
 
         deck.add(Card(None, None, "all", True))
         count += 1
-    print(f"count : {count}")
     temp_list = deck.sprites()
     random.shuffle(temp_list)
     pop_card = temp_list.pop()
-    print(f"{pop_card.color} / {pop_card.number} / {pop_card.skill}")
     deck.remove(pop_card)
     remain.add(pop_card)
+
+    now_card = pop_card
     now_card_surf = pop_card.image
     now_card_rect = now_card_surf.get_rect(center=(400 + (now_card_surf.get_width() // 2), 300))
 
 
+# turn_index를 더 깔끔하게 다루기
 def draw_from_center(input_deck):
-    global deck, turn_index
-    pop_card = deck.sprites().pop()
-    deck.remove(pop_card)
-    input_deck.add(pop_card, layer=0)
-
-    if turn_index == 0:
-        pop_card.rect.y = 450  # 200 - rect_height // 2
-        pop_card.initial_y = 450
-    elif turn_index == 1:
-        pop_card.rect.y = 50  # 200 - rect_height // 2
-        pop_card.initial_y = 50
-    else:
-        turn_index = 0
-
+    global deck, turn_index, is_get
+    draw_card(input_deck)
     hand_update(input_deck)
+    is_get = True
 
-    turn_index += 1
-
-    # layer를 다시 수정해주는 작업
-    for i, card in enumerate(input_deck.sprites()):
-        # input_deck.change_layer(card, len(input_deck.sprites()) - i - 1)
-        input_deck.change_layer(card, i)
+    # # layer를 다시 수정해주는 작업
+    # for i, card in enumerate(input_deck.sprites()):
+    #     # input_deck.change_layer(card, len(input_deck.sprites()) - i - 1)
+    #     input_deck.change_layer(card, i)
 
 
 def player_card_setting(input_deck):
@@ -160,19 +173,7 @@ def player_card_setting(input_deck):
     print(len(deck.sprites()))
     if not len(input_deck):  # 초기에 7장 뽑기
         for i in range(7):
-
-            temp_list = deck.sprites()
-            random.shuffle(temp_list)
-            pop_card = temp_list.pop()
-            deck.remove(pop_card)
-            input_deck.add(pop_card, layer=i)
-            if turn_index == 0:
-                pop_card.rect.y = 450  # 200 - rect_height // 2
-                pop_card.initial_y = 450
-            elif turn_index == 1:
-                pop_card.rect.y = 50  # 200 - rect_height // 2
-                pop_card.initial_y = 50
-
+            draw_card(input_deck)
     hand_update(input_deck)
 
     # 레이어 재정렬 - add()의 layer가 차례대로 쌓이기 때문에 역순으로 다시 재정렬 해줘야함
@@ -181,27 +182,19 @@ def player_card_setting(input_deck):
         input_deck.change_layer(card, i)
 
 
-def check_turn():
-    global turn_index, turn_list, PLAYER_NUMBER
-    turn_index += 1
-    print(len(turn_list))
-    if turn_index == 2:
-        print("clear turn_index")
-        turn_index = 0
-    print(f"after check_turn(): {turn_index}")
 
 
 def check_condition(input_card):
     # input 카드가 현재 맨 위에 있는 카드에 낼 수 있는 카드인지 확인하는 함수
     global remain
-    now = remain.sprites()[-1]
+    now = now_card
     print(f"input.color : {input_card.color} / now.color : {now.color}")
     print(f"input.skill : {input_card.skill} / now.skill : {now.skill}")
     print(f"input.number : {input_card.number} / now.number : {now.number}")
     if input_card.is_wild or now.is_wild:
         return True
 
-    if input_card.color == now.color: # yellow none 9 / blue none 5
+    if input_card.color == now.color:  # yellow none 9 / blue none 5
         return True
     elif input_card.number == now.number:
         if input_card.color is not None and input_card.skill is not None:
@@ -209,7 +202,7 @@ def check_condition(input_card):
                 return True
             return False
         return True
-    elif input_card.skill == now.skill: # yellow none 9 / blue none 5
+    elif input_card.skill == now.skill:  # yellow none 9 / blue none 5
         if input_card.color is not None and input_card.number is not None:
             return False
         return True
@@ -227,24 +220,27 @@ def skill_active(skill):
         reverse_turn()
     elif skill == "block":
         block_turn()
-    elif skill == "change":
+    elif skill == "change" or skill == "all":
         change_color()
     elif skill == "plus2":
-        for _ in range(2):
-            draw_from_center(turn_list[next_player])
-    elif skill == "plus4":
-        for _ in range(4):
-            draw_from_center(turn_list[next_player])
+        draw_two(turn_list[next_player], next_player)
+    elif skill == "plus4" or skill == "all4":
+        draw_four(turn_list[next_player], next_player)
 
+def pass_turn():
+    global turn_index, turn_list
+    print("turn is passed")
+    turn_index += 1
+    if turn_index == len(turn_list):
+        turn_index = 0
 
 
 def start_single_play():
-    global turn_list, turn_index, deck, now_card_surf, now_card_rect, is_color_change, game_active
+    global turn_list, turn_index, deck, now_card_surf, now_card_rect, is_color_change, game_active, is_get, now_card
     run = True
-
-
+    mouse_check = False
     while run:
-        screen.fill((50,200,50))
+        screen.fill((50, 200, 50))
 
         # event loop
 
@@ -271,7 +267,7 @@ def start_single_play():
             else:
                 pass
             # 카드에 마우스커서를 올렸을 때 애니메이션 > 리팩토링
-            if event.type == pygame.MOUSEMOTION and game_active:
+            if event.type == pygame.MOUSEMOTION and game_active and not is_color_change:
                 for card in turn_list[turn_index].sprites():
                     if card.rect.collidepoint(event.pos):
                         # print(turn_list[turn_index].get_sprites_at(event.pos)[0].rect.left)
@@ -301,19 +297,22 @@ def start_single_play():
                 print("button down")
 
             # is_color_change에 따라 색깔을 바꿔주는 옵션
-            if is_color_change and event.type== pygame.MOUSEBUTTONDOWN:
+            if is_color_change and event.type == pygame.MOUSEBUTTONDOWN:
                 for color_list in change_color_list:
                     if color_list[1].collidepoint(event.pos):
                         print("color chagne active")
                         now_card_surf = pygame.image.load(
                             f"resources/images/card/normalMode/change/{color_list[2]}_change.png").convert_alpha()
                         now_card_surf = pygame.transform.scale(now_card_surf, (70, 100))
+                        now_card.color = color_list[2]
                         is_color_change = False
+                        pass_turn()
 
             # turn_index를 이용해 게임 flow control
             # 플레이어 턴에 플레이어가 할 수 있는 행동
+            if event.type == pygame.MOUSEBUTTONDOWN and game_active and not is_color_change:
+
             # 1. 낼 수 있는 카드를 낸다
-            if event.type == pygame.MOUSEBUTTONDOWN and game_active:
                 for card in turn_list[turn_index].sprites():
                     if card.rect.collidepoint(event.pos):
                         if check_condition(card):
@@ -322,16 +321,24 @@ def start_single_play():
                             pop_card = card
                             turn_list[turn_index].remove(card)
                             remain.add(pop_card)
+                            now_card = pop_card
                             now_card_surf = pop_card.image
                             hand_update(turn_list[turn_index])
-                            check_turn()
-            # 1-1. 낸 카드의 능력이 있다면 해당 카드의 능력을 수행해야 한다
+                            # 1-1. 낸 카드의 능력이 있다면 해당 카드의 능력을 수행해야 한다
                             if card.skill is not None:
                                 skill_active(card.skill)
+                            if card.skill != 'change':
+                                pass_turn()
             # 2. 가운데에서 카드를 가져온다 > 낼 수 있는 카드가 있다면 낸다
-            # 3. 컴퓨터의 알고리즘 수행
-            # 4. 카드가 1장만 남았을 경우 UNO 버튼을 눌러야 한다.
-            # 5. 누군가의 덱이 모두 사라지면 그 사람의 승리 > 승리 화면 전환 > 메인 화면 전환
+                if deck_rect.collidepoint(event.pos) and not is_get:
+                    draw_from_center(turn_list[turn_index])
+            # 3. 낼 수 있는 카드가 없거나, 가운데에서 이미 카드를 가져온 상태면 PASS를 눌러 턴을 넘김
+                if is_get and skip_rect.collidepoint(event.pos):
+                    pass_turn()
+                    is_get = False
+            # 4. 컴퓨터의 알고리즘 수행
+            # 5. 카드가 1장만 남았을 경우 UNO 버튼을 눌러야 한다.
+            # 6. 누군가의 덱이 모두 사라지면 그 사람의 승리 > 승리 화면 전환 > 메인 화면 전환
 
         # event loop 종료 *****************************
 
@@ -357,7 +364,12 @@ def start_single_play():
                 screen.blit(alpha_surface, (0, 0))
                 for color_list in change_color_list:
                     screen.blit(color_list[0], color_list[1])
-
+            if is_get:
+                pygame.draw.rect(screen, (200,200,200), skip_rect, 0)
+                screen.blit(skip_surf, skip_rect)
+            else:
+                pygame.draw.rect(screen, (64, 64, 64), skip_rect, 0)
+                screen.blit(skip_surf, skip_rect)
         else:
             screen.fill("green")
             # 게임이 종료되었을 때 덱 초기화
@@ -403,6 +415,7 @@ deck_rect = deck_surf.get_rect(center=(400 - (deck_surf.get_width() // 2), 300))
 # 현재 어떤카드가 뒤집혀져있는지 나타내는 변수 / 추후 수정해야 함
 # 1. 낸 카드로 이미지가 바뀌게
 # 2.
+now_card = Card('red', None, 0, False)
 now_card_surf = pygame.image.load("resources/images/output.png").convert_alpha()
 # now_card_surf = pygame.transform.rotozoom(now_card_surf, 0, 0.5)
 now_card_rect = now_card_surf.get_rect(center=(400 + (now_card_surf.get_width() // 2), 300))
@@ -410,28 +423,30 @@ now_card_rect = now_card_surf.get_rect(center=(400 + (now_card_surf.get_width() 
 now_turn_surf = font.render(f"Player{turn_index + 1}'s turn", False, (64, 64, 64))
 now_turn_surf2 = font.render(f"Player{turn_index + 2}'s turn", False, (64, 64, 64))
 now_turn_rect = now_turn_surf.get_rect(center=(100, 300))
-now_color = "none"  # 현재 내야하는 색깔
 
 CENTER_X_POS = 625
 CENTER_Y_POS = 325
-red_surf = pygame.Surface((50,50))
-red_surf.fill((255,0,0))
-red_rect = red_surf.get_rect(center=(CENTER_X_POS - 25,CENTER_Y_POS - 25))
+red_surf = pygame.Surface((50, 50))
+red_surf.fill((255, 0, 0))
+red_rect = red_surf.get_rect(center=(CENTER_X_POS - 25, CENTER_Y_POS - 25))
 green_surf = pygame.Surface((50, 50))
-green_surf.fill((0,255,0))
+green_surf.fill((0, 255, 0))
 green_rect = green_surf.get_rect(center=(CENTER_X_POS + 25, CENTER_Y_POS - 25))
 blue_surf = pygame.Surface((50, 50))
-blue_surf.fill((0,0,255))
+blue_surf.fill((0, 0, 255))
 blue_rect = blue_surf.get_rect(center=(CENTER_X_POS - 25, CENTER_Y_POS + 25))
 yellow_surf = pygame.Surface((50, 50))
-yellow_surf.fill((255,255,0))
+yellow_surf.fill((255, 255, 0))
 yellow_rect = yellow_surf.get_rect(center=(CENTER_X_POS + 25, CENTER_Y_POS + 25))
-change_color_list = []
-
+change_color_list = [[red_surf, red_rect, "red"], [green_surf, green_rect, 'green'], [blue_surf, blue_rect, 'blue'],
+                     [yellow_surf, yellow_rect, 'yellow']]
 is_color_change = False
 
 alpha_surface = pygame.Surface((screen.get_width(), screen.get_height()), pygame.SRCALPHA)
 alpha_surface.fill((0, 0, 0, 128))
+
+skip_surf = font.render("PASS", False, (64,64,64))
+skip_rect = skip_surf.get_rect(center=(520,350))
 # 카드 생성과 분배
 # 카드 덱을 만들고, 플레이어들에게 무작위로 카드를 분배
 
@@ -447,7 +462,7 @@ card_height = 100
 
 # 카드 놓기
 # 플레이어는 자신의 카드 중 현재 카드와 색상 또는 숫자가 일치하는 카드를 놓을 수 있음
-
+is_get = False
 
 # 특수 카드 처리
 # 특수 카드인 Skip, Reverse, Draw Two, Wild, Wild Draw Four 등에 대한 처리를 구현합니다.
